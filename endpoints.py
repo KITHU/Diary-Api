f36687779cedad42b707cec6cb0ae03101c10ba1
KITHU <james.kithu2@gmail.com>
2018-07-20 18:45:54 +0300

    [feature#159199593] added GET endpoint to fetch one entry

--- endpoints.py
+++ endpoints.py
@@ -15,6 +15,14 @@ def get_all_diary_entries():
     """Tis method will return all diary data stored"""
     return jsonify({'diary_db': diary_db})
 
+@APP.route('/mydiary/v1/diaryentries/<int:entry_id>', methods=['GET'])
+def get_one_diary_entry(entry_id):
+    """this method will return one diary entry"""
+    diary_entry = [diary_entry for diary_entry in diary_db if diary_entry['id'] == entry_id]
+    if not diary_entry:
+        return jsonify({'error':'expect title to be string'}),404
+    return jsonify({'diary_entry': diary_entry[0]})
+
 
 if __name__ == '__main__':
     APP.run(debug=True)
