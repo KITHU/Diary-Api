47f0cbb4b6b9c232c565699cf9b0c0223dda124a
KITHU <james.kithu2@gmail.com>
2018-08-02 10:10:00 +0300

    uncommented online database declaration

--- dbconnection.py
+++ dbconnection.py
@@ -2,36 +2,36 @@
 import datetime
 import psycopg2
 
-# def connection():
-#     """establish connection to database """
-#     try:
-#         conn = psycopg2.connect(
-#             database='d7f3q9oc94flbp',
-#             user='eurwbxjvautgcf',
-#             host='ec2-23-23-242-163.compute-1.amazonaws.com',
-#             password='4497fbe94664feb676f14fbbc38d90d910563bb0b54c82ee99c099dab5fb2a84'
-#             )
-#     except: # pylint: disable=bare-except
-#         print("I am unable to connect to the database")
-#     return conn
+def connection():
+    """establish connection to database """
+    try:
+        conn = psycopg2.connect(
+            database='d7f3q9oc94flbp',
+            user='eurwbxjvautgcf',
+            host='ec2-23-23-242-163.compute-1.amazonaws.com',
+            password='4497fbe94664feb676f14fbbc38d90d910563bb0b54c82ee99c099dab5fb2a84'
+            )
+    except: # pylint: disable=bare-except
+        print("I am unable to connect to the database")
+    return conn
 
-# def commit_closedb(dbconn):
-#     "used to close and commit changes"
-#     dbconn.commit()
-#     dbconn.close()
+def commit_closedb(dbconn):
+    "used to close and commit changes"
+    dbconn.commit()
+    dbconn.close()
 
 dt = str(datetime.datetime.now().date()) # pylint: disable=invalid-name
 
 
-def connection():
-    """establish connection to database """
-    try:
-        conn = psycopg2.connect("dbname='my_diary' user='root' host='localhost' password='root2'")
-    except:
-        print("I am unable to connect to the database")
-    print("connected")
-    return conn
+# def connection():
+#     """establish connection to database """
+#     try:
+#         conn = psycopg2.connect("dbname='my_diary' user='root' host='localhost' password='root2'")
+#     except:
+#         print("I am unable to connect to the database")
+#     print("connected")
+#     return conn
 
-def commit_closedb(dbconn):
-    dbconn.commit()
-    dbconn.close()
+# def commit_closedb(dbconn):
+#     dbconn.commit()
+#     dbconn.close()
