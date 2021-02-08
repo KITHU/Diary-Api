449268c1dab5100275017dd5f6e8e3fc85ba3bce
KITHU <james.kithu2@gmail.com>
2021-02-08 07:15:44 +0300

    add test

--- tests/test_users.py
+++ tests/test_users.py
@@ -12,6 +12,16 @@ class TestUsersEdpoints:
         response = client.post('api/v1/auth/signup',data=json.dumps(data))
         assert response.status_code == 201
 
+    def test_creat_new_user_fails(self, client, init_db):
+        # creating new user fails if any of required fields is left black
+        data = {
+                'first_name':'mwaniki',
+                'last_name':'muriuki',
+                'email':'mwaniki.muriuki@gmail.com'
+            }
+        response = client.post('api/v1/auth/signup',data=json.dumps(data))
+        assert response.status_code == 400
+
 
     def test_signin_user_pass(self, client, init_db):
         # test app ability to sigin existing user
@@ -22,3 +32,13 @@ class TestUsersEdpoints:
             }
         response = client.post('api/v1/auth/signin',data=data)
         assert response.status_code == 200
+    
+    def test_signin_user_fails(self, client, init_db):
+        # test app ability to sigin existing user
+        #with incorrect email or password
+        data = {
+                'email':'weru.muriuki@gmail.com',
+                'password':'1234r'
+            }
+        response = client.post('api/v1/auth/signin',data=data)
+        assert response.status_code == 400
